7fadb12b92aedcdfa1325620da738520be1e0ee0
Ilya Kubariev <kubarievilya@gmail.com>
2021-08-08 00:42:35 +0300

    1:13:33

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
-import {CardComponent} from "./card/card.component";
+import { CardComponent } from "./card/card.component";
 import { FormComponent } from './form/form.component';
 
 @NgModule({
@@ -12,7 +13,8 @@ import { FormComponent } from './form/form.component';
     FormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,10 +1,26 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+  title: string = 'My Card Title'
+  text: string = 'My Sample Text'
+
+  ngOnInit(): void { }
+
+  changeTitle() {
+    this.title = 'Title has been changed'
+  }
+
+  inputHandler(value: any) {
+    this.title = value
+  }
+
+  changeHandler() {
+    console.log(this.title)
+  }
 
 }
